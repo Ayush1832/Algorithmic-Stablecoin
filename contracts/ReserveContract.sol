e2ab338ba1f1e8546ab4341617c8b9e59f1c9153
Ayush Nayak <nayaksubham18@gmail.com>
2024-09-06 12:15:33 +0530

    update

--- contracts/ReserveContract.sol
+++ contracts/ReserveContract.sol
@@ -3,11 +3,10 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract USDReserves is Ownable, ReentrancyGuard, AccessControl {
